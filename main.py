6d6b28ed524207c64b669abf1bd6341e26063035
Parvej Aalam <45516687-aalamp140@users.noreply.replit.com>
2025-08-17 14:39:09 +0000

    Initial commit - MediCare

--- main.py
+++ main.py
@@ -373,6 +373,4 @@ def admin_dashboard():
 
 # ---------------- RUN SERVER -----------------
 if __name__=="__main__":
-    import os
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host="0.0.0.0", port=port, port=8080, debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
